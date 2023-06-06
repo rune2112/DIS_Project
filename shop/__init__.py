75b9146a8cd5d2356769642e8e3f7d620bc1e8c1
Rune Jørgensen <rune2112@gmail.com>
2023-06-06 11:44:56 +0200

    DB_credentials

--- shop/__init__.py
+++ shop/__init__.py
@@ -3,12 +3,15 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import psycopg2
 import os
+import json
 
 app = Flask(__name__)
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
-#db = "dbname='postgres' user='postgres' host='127.0.0.1' password = 'postgresPassword'"
-db = "dbname='DIS_Project' user='postgres' host='127.0.0.1' password = 'DISProjectPass'"
+f = open("shop/DB_cred.json", "r")
+cred = json.load(f)
+f.close()
+db = f"dbname={cred['dbname']} user={cred['user']} host={cred['host']} password = {cred['password']}"
 conn = psycopg2.connect(db)
 
 bcrypt = Bcrypt(app)
